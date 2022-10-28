58fd6b2fb2b3789b2e11b62eb37dcec690d6da6d
wolstadxd <wolski.rd@gmail.com>
2022-10-28 20:47:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+    const [fromCurrency, setFromCurrency] = useState('RUB')
+    const [toCurrency, setToCurrency] = useState('USD')
+    const [fromPrice, setFromPrice] = useState(0)
+    const [toPrice, setToPrice] = useState(0)
+    const ratesRef = useRef({})
+
+    useEffect(() => {
+        fetch('https://cdn.cur.su/api/latest.json').then((res) => res.json()).then((json) => {
+            ratesRef.current = json.rates
+            onChangeToPrice(1)
+        }).catch(err => {
+            console.warn(err)
+            alert('Can not resolve information')
+        })
+    }, [])
+
+    const onChangeFromPrice = (value) => {
+        const price = value / ratesRef.current[fromCurrency]
+        const result = price * ratesRef.current[toCurrency]
+        setToPrice(result.toFixed(3))
+        setFromPrice(value)
+    }
+
+    const onChangeToPrice = (value) => {
+        const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+        setFromPrice(result.toFixed(3))
+        setToPrice(value)
+    }
+
+    // const onChangeFromCurrency = (cur) => {
+    //     setFromCurrency(cur)
+    //     onChangeFromPrice(fromPrice)
+    // }
+
+    useEffect(() => {
+        onChangeFromPrice(fromPrice)
+    }, [fromCurrency])
+
+    useEffect(() => {
+        onChangeToPrice(toPrice)
+    }, [toCurrency])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block value={fromPrice}
+             currency={fromCurrency}
+             onChangeCurrency={setFromCurrency}
+             onChangeValue={onChangeFromPrice}
+      />
+      <Block
+          value={toPrice}
+          currency={toCurrency}
+          onChangeCurrency={setToCurrency}
+          onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
